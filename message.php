114dcee3479465cf2821a4a45bd7e0fdc622de77
Артур <admin@dinamikasveta.com>
2017-07-22 11:53:40 +0500

    second

--- message.php
+++ message.php
@@ -10,11 +10,11 @@ if(!$email && !$phone){
 	echo 'Укажите номер телефона или email';
 	exit();
 }
-$to      = 'v628966@yandex.ru';
+$to      = 'otwintanet@yandex.ru';
 $subject = 'Заявка на выкуп авто';
-$message = 'Заявка на выкуп автомобиля с сайта: Имя:'.$name.'. Номер телефон:'.($phone ? $phone : '').'. Email:'.($email ? $email : '').'. Марка автомобиля:'.$auto_mark.'. Модель автомобиля:'.$auto_model.'. Год выпуска:'.$auto_year.'. Описание авто:'.($auto_info ? $auto_info : '').'. Желаемая цена:'.($auto_price ? $auto_price : '').'.';
-$headers = 'From: site@628966.ru' . "\r\n" .
-    'Reply-To: site@628966.ru' . "\r\n" .
+$message = 'Заявка на выкуп автомобиля с сайта: Имя:'.$name.'. Город: '.($city ? $city : '').'. Номер телефон:'.($phone ? $phone : '').'. Email:'.($email ? $email : '').'. Марка автомобиля:'.$auto_mark.'. Модель автомобиля:'.$auto_model.'. Год выпуска:'.$auto_year.'. Описание авто:'.($auto_info ? $auto_info : '').'. Желаемая цена:'.($auto_price ? $auto_price : '').'.';
+$headers = 'From: site@otwinta.net' . "\r\n" .
+    'Reply-To: site@otwinta.net' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 if(mail($to, $subject, $message, $headers)){
