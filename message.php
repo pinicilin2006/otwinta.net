0fa4d270d39dad34cc04f59f7783bee7729e3331
Артур <admin@dinamikasveta.com>
2017-07-22 12:08:54 +0500

    fix

--- message.php
+++ message.php
@@ -12,7 +12,7 @@ if(!$email && !$phone){
 }
 $to      = 'otwintanet@yandex.ru';
 $subject = 'Заявка на выкуп авто';
-$message = 'Заявка на выкуп автомобиля с сайта: Имя:'.$name.'. Город: '.($city ? $city : '').'. Номер телефон:'.($phone ? $phone : '').'. Email:'.($email ? $email : '').'. Марка автомобиля:'.$auto_mark.'. Модель автомобиля:'.$auto_model.'. Год выпуска:'.$auto_year.'. Описание авто:'.($auto_info ? $auto_info : '').'. Желаемая цена:'.($auto_price ? $auto_price : '').'.';
+$message = 'Заявка на выкуп автомобиля с сайта: Имя:'.$name.'.\n Город: '.($city ? $city : '').'. Номер телефон:'.($phone ? $phone : '').'. Email:'.($email ? $email : '').'. Марка автомобиля:'.$auto_mark.'. Модель автомобиля:'.$auto_model.'. Год выпуска:'.$auto_year.'. Описание авто:'.($auto_info ? $auto_info : '').'. Желаемая цена:'.($auto_price ? $auto_price : '').'.';
 $headers = 'From: site@otwinta.net' . "\r\n" .
     'Reply-To: site@otwinta.net' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
