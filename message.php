a08ab62526cd14c8816aa898dd6b83d6885b5355
Артур <admin@dinamikasveta.com>
2017-07-22 12:19:37 +0500

    fi

--- message.php
+++ message.php
@@ -12,13 +12,13 @@ if(!$email && !$phone){
 }
 $to      = 'otwintanet@yandex.ru';
 $subject = 'Заявка на выкуп авто';
-$message = 'Заявка на выкуп автомобиля с сайта: Имя:'.$name.'.<br> Город: '.($city ? $city : '').'. Номер телефон:'.($phone ? $phone : '').'. Email:'.($email ? $email : '').'. Марка автомобиля:'.$auto_mark.'. Модель автомобиля:'.$auto_model.'. Год выпуска:'.$auto_year.'. Описание авто:'.($auto_info ? $auto_info : '').'. Желаемая цена:'.($auto_price ? $auto_price : '').'.';
+$message = 'Заявка на выкуп автомобиля с сайта otwinta.net:<br> Имя:'.$name.'.<br> Город: '.($city ? $city : '').'.<br> Номер телефона:'.($phone ? $phone : '').'.<br> Email:'.($email ? $email : '').'.<br> Марка автомобиля:'.$auto_mark.'. <br>Модель автомобиля:'.$auto_model.'.<br> Год выпуска:'.$auto_year.'.<br> Описание авто:'.($auto_info ? $auto_info : '').'.<br> Желаемая цена:'.($auto_price ? $auto_price : '').'.';
 $headers = 'From: site@otwinta.net' . "\r\n" .
     'Reply-To: site@otwinta.net' . "\r\n" .
     'Content-type: text/html' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 if(mail($to, $subject, $message, $headers)){
-	echo 'Заявка успешно отправлена! С Вами свяжутся в ближайшее время.';
+	echo '<b>Заявка успешно отправлена! С Вами свяжутся в ближайшее время.</b>';
 }
 ?>
