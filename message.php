99224020d54badf86a242e8e7e57227854ded364
Артур <admin@dinamikasveta.com>
2017-07-22 12:21:26 +0500

    fix

--- message.php
+++ message.php
@@ -12,7 +12,7 @@ if(!$email && !$phone){
 }
 $to      = 'otwintanet@yandex.ru';
 $subject = 'Заявка на выкуп авто';
-$message = 'Заявка на выкуп автомобиля с сайта otwinta.net:<br> Имя:'.$name.'.<br> Город: '.($city ? $city : '').'.<br> Номер телефона:'.($phone ? $phone : '').'.<br> Email:'.($email ? $email : '').'.<br> Марка автомобиля:'.$auto_mark.'. <br>Модель автомобиля:'.$auto_model.'.<br> Год выпуска:'.$auto_year.'.<br> Описание авто:'.($auto_info ? $auto_info : '').'.<br> Желаемая цена:'.($auto_price ? $auto_price : '').'.';
+$message = 'Заявка на выкуп автомобиля с сайта otwinta.net:<br> Имя:'.$name.'.<br> Город: '.($city ? $city : '').'.<br> Номер телефона:'.($phone ? $phone : '').'.<br> Email:'.($email ? $email : '').'.<br> Марка автомобиля:'.$auto_mark.'. <br>Модель автомобиля:'.$auto_model.'.<br> Год выпуска:'.$auto_year.'.<br> Описание авто:'.($auto_info ? $auto_info : '').'.<br><br> Желаемая цена:'.($auto_price ? $auto_price : '').'.';
 $headers = 'From: site@otwinta.net' . "\r\n" .
     'Reply-To: site@otwinta.net' . "\r\n" .
     'Content-type: text/html' . "\r\n" .
